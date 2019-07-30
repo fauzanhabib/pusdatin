cf51fad9f8c884a566dd583f1951acf7733b9a27
Dzulfaqar Aar <dzulfaqar.arab@gmail.com>
2019-07-30 22:34:52 +0700

    Initial commit

---
+++ app/Model/Bdt/BdtStatusKeberadaan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Bdt;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class BdtStatusKeberadaan extends Model
+{
+    
+}
---
+++ app/Model/Bdt/BdtStatusPekerjaan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Bdt;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class BdtStatusPekerjaan extends Model
+{
+    
+}
