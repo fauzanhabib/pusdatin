54587592b3e63fe75407a93438c2bc4a89d54361
Dzulfaqar Aar <dzulfaqar.arab@gmail.com>
2019-08-12 00:00:21 +0700

    update

--- config/voyager.php
+++ config/voyager.php
@@ -186,7 +186,7 @@ return [
     |
     */
 
-    'primary_color' => '#05205D',
+    'primary_color' => '#22A7F0',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
